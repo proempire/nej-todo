210a5e086d35166393dbafeeb3b0e0eafd0243fb
郑正和 <zhengzhenghe@corp.netease.com>
2019-04-09 15:02:50 +0800

    feat: name sync

--- server/routes/item.js
+++ server/routes/item.js
@@ -4,20 +4,40 @@ const router = Router();
 
 router.get('items', async (req, res) => {
     const items = await req.context.models.Item.find();
+
     return res.send(items);
 });
 
 router.post('items/add', async (req, res) => {
-    const items = await req.context.models.Item.find();
-    return res.send(items);
+    const item = await req.context.models.Item.findById(
+        req.body.id
+    );
+
+    let result = null;
+    if (item) {
+        result = item.update({ $inc: { volume: 1 } }).exec();
+    }
+
+    return res.send(result);
 });
 
 router.post('items/delete', async (req, res) => {
-    const items = await req.context.models.Item.find();
-    return res.send(items);
+    const item = await req.context.models.Item.findById(
+        req.body.id
+    );
+
+    let result = null;
+    if (item) {
+        result = await item.remove();
+    }
+
+    return res.send(item);
 });
 
 router.post('items/new', async (req, res) => {
-    const items = await req.context.models.Item.find();
-    return res.send(items);
+    const item = await req.context.models.Item.create({
+        name: req.body.name
+    });
+
+    return res.send(item);
 });
